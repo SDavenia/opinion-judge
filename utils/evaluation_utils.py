e305f9739289aff8ed85e6811d55e3284b529c7d
Marco Madeddu <marco.madeddu.bra@gmail.com>
2026-09-02 14:41:48 +0000

    added implementation of rs and pc from df

--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 import math 
+import pandas as pd
 
 def get_den_normalization(n:int, range_size:float):
 
@@ -38,4 +39,35 @@ def calculate_pc(scores: list[list[tuple[float, float]]],
     pc = 1 -((1/n) * tot_sum)
     return pc
 
+def _get_grouped_scores_rs(df_scoring: pd.DataFrame,
+                        score_column: str) -> list:
+    result = (
+        df_scoring.groupby(["id_1", "id_2"])
+        .agg(lambda x: x.tolist())
+        [score_column]
+        .tolist()
+    )
+    return result
 
+def calculate_rs_from_df(df_scoring: pd.DataFrame,
+                         range: tuple[float, float] = (0.0,1.0)) -> float:
+
+    sc_1_to_2 = _get_grouped_scores_rs(df_scoring, "parsed_score_1to2")
+    sc_2_to_1 = _get_grouped_scores_rs(df_scoring, "parsed_score_2to1")
+    all_scores = sc_1_to_2 + sc_2_to_1
+    return calculate_rs(all_scores, range=range)
+
+def _get_grouped_scores_pc(df_scoring: pd.DataFrame) -> list:
+    result = (df_scoring.groupby(["id_1", "id_2"])
+       .agg(lambda x: x.tolist())
+       .apply(lambda row: list(zip(row["parsed_score_1to2"], row["parsed_score_2to1"])), axis=1)
+       .tolist()
+    )
+    return result
+    #domanda che mi sto facendo, ma in pc ci importa farlo raggruppando? non ha senso prendere tutto assieme?
+
+def calculate_pc_from_df(df_scoring: pd.DataFrame,
+                         range: tuple[float, float] = (0.0,1.0)) -> float:
+
+    scores_pc = _get_grouped_scores_pc(df_scoring)
+    return calculate_pc(scores_pc, range=range)
